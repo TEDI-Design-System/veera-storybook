9ee6e8573c247affcc87f253b7987955297eebbd
pearu.sarv <pearu.sarv@twn.ee>
2023-08-07 12:21:33 +0300

    VEER-35 Add color utility classes

---
+++ scripts/constants.js
@@ -0,0 +1,6 @@
+import { scssContants } from '../constants.js';
+
+const prefix = scssContants['veera-prefix'];
+const autoGenWarning = '// NB! This file is auto generated\n\n';
+
+export { prefix, autoGenWarning };
