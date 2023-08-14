b1c8e935e872093f9e71d806d3726d038fd40bd9
pearu.sarv <pearu.sarv@twn.ee>
2023-08-14 13:11:02 +0300

    VEER-55
    Add label and lock icon to switch

--- src/stories/forms/switch/switch.stories.ts
+++ src/stories/forms/switch/switch.stories.ts
@@ -1,19 +1,35 @@
-import type { Meta, StoryObj } from '@storybook/html';
-import { SwitchProps, createSwitch } from './switch';
+import type { StoryObj } from '@storybook/html';
+import { SwitchProps, SwitchWithLabelProps, createSwitch, createSwitchWithLabel } from './switch';
 
 const meta = {
   title: 'forms/switch',
   tags: ['autodocs'],
-  render: createSwitch,
   argTypes: {
     disabled: { type: 'boolean' },
+    hasIcon: { type: 'boolean' },
   },
-} satisfies Meta<SwitchProps>;
+};
 
 export default meta;
 
-type Story = StoryObj<SwitchProps>;
+type Story<T> = StoryObj<T>;
 
-export const Default: Story = {
+export const Default: Story<SwitchProps> = {
+  render: createSwitch,
   args: {},
+};
+
+export const WithLock: Story<SwitchProps> = {
+  render: createSwitch,
+  args: {
+    hasIcon: true,
+  },
+};
+
+export const WithLabel: Story<SwitchWithLabelProps> = {
+  render: createSwitchWithLabel,
+  args: {
+    hasIcon: true,
+    label: 'Switch',
+  },
 };
