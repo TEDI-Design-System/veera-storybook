398688ace8f366a396dcdecc80eda267f954f372
pearu.sarv <pearu.sarv@twn.ee>
2023-09-13 10:55:52 +0300

    VEER-58
    Add accordion

--- src/stories/utils.ts
+++ src/stories/utils.ts
@@ -6,3 +6,11 @@ export type NumericRange<
 > = ARR['length'] extends END
   ? ACC | START | END
   : NumericRange<START, END, [...ARR, 1], ARR[START] extends undefined ? ACC : ACC | ARR['length']>;
+
+export const createContentFill = () => {
+  const content = document.createElement('div');
+  content.style.height = '200px';
+  content.style.border = '1px dashed lightgray';
+  content.style.borderRadius = '4px';
+  return content;
+};
