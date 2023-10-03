a96d33e4fcc7e6dfc4e6f7d09a0f10fda5cb4afc
Priit Puru <priit.puru@twn.ee>
2023-10-03 13:33:17 +0300

    Add robots.txt
    Add preliminary release script

---
+++ scripts/release.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+
+const release = () => {
+    const sourceFolder = "dist";
+    const parentFolder = "release";
+    const folderName = `veera@${process.env.npm_package_version}`;
+    try {
+        if (!fs.existsSync(parentFolder)) {
+            fs.mkdirSync(parentFolder);
+        }
+        fs.cpSync(sourceFolder, `${parentFolder}/${folderName}`, { recursive: true });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+release();
