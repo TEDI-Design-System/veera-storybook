a2b24b06bca6ed6c143a6f6d6951121433c09a39
pearu.sarv <pearu.sarv@twn.ee>
2023-10-09 12:57:24 +0300

    Add large accordion variant

--- src/stories/components/accordion/accordion.stories.ts
+++ src/stories/components/accordion/accordion.stories.ts
@@ -4,6 +4,12 @@ import { AccordionStoryProps, createAccordion } from './accordion';
 const meta = {
   title: 'components/accordion',
   tags: ['autodocs'],
+  argTypes: {
+    size: {
+      control: { type: 'select' },
+      options: ['sm', 'lg'],
+    },
+  },
   render: createAccordion,
 } satisfies Meta<AccordionStoryProps>;
 
@@ -12,5 +18,6 @@ export default meta;
 export const Accordion: StoryObj<AccordionStoryProps> = {
   args: {
     disabled: false,
+    size: 'sm',
   },
 };
