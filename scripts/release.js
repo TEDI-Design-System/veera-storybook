c1800fd6f6286c976e34da9d538e41d834d9bd2a
Priit Puru <priit.puru@twn.ee>
2023-10-22 22:44:12 +0300

    Change release script

--- scripts/release.js
+++ scripts/release.js
@@ -7,12 +7,14 @@ const release = () => {
   const versionFolder = `${process.env.npm_package_version}`;
   const scssFolder = `${releaseFolder}/${versionFolder}/scss`;
   try {
-    if (!fs.existsSync(releaseFolder)) {
-      fs.mkdirSync(releaseFolder);
+    if (fs.existsSync(releaseFolder)) {
+      fs.rmSync(releaseFolder, { recursive: true, force: true });
     }
+    fs.mkdirSync(releaseFolder);
     fs.cpSync(distFolder, `${releaseFolder}/${versionFolder}`, { recursive: true });
     fs.mkdirSync(`${scssFolder}`);
     fs.cpSync(`${srcFolder}/scss`, scssFolder, { recursive: true });
+    fs.cpSync(`design-tokens.json`, `${releaseFolder}/${versionFolder}/design-tokens.json`);
   } catch (err) {
     console.error(err);
   }
