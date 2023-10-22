e45b5134fef66563c68a74b9c34dea7d0f24a8b7
Priit Puru <priit.puru@twn.ee>
2023-10-22 22:16:28 +0300

    Update release script to pack scss

--- scripts/release.js
+++ scripts/release.js
@@ -1,17 +1,21 @@
 import fs from 'fs';
 
 const release = () => {
-    const sourceFolder = "dist";
-    const parentFolder = "release";
-    const folderName = `${process.env.npm_package_version}`;
-    try {
-        if (!fs.existsSync(parentFolder)) {
-            fs.mkdirSync(parentFolder);
-        }
-        fs.cpSync(sourceFolder, `${parentFolder}/${folderName}`, { recursive: true });
-    } catch (err) {
-        console.error(err);
+  const distFolder = 'dist';
+  const srcFolder = 'src';
+  const releaseFolder = 'release';
+  const versionFolder = `${process.env.npm_package_version}`;
+  const scssFolder = `${releaseFolder}/${versionFolder}/scss`;
+  try {
+    if (!fs.existsSync(releaseFolder)) {
+      fs.mkdirSync(releaseFolder);
     }
+    fs.cpSync(distFolder, `${releaseFolder}/${versionFolder}`, { recursive: true });
+    fs.mkdirSync(`${scssFolder}`);
+    fs.cpSync(`${srcFolder}/scss`, scssFolder, { recursive: true });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 release();
