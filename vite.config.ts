e68a2fb4a46cc14c0d09462d019f5a58b2d2f00c
pearu.sarv <pearu.sarv@twn.ee>
2023-11-17 11:12:51 +0200

    Update build script Remove unnecessary files Self host fonts

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,6 @@ export default defineConfig({
     },
   },
   build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'index.html'),
-      name: 'e-service-design-system',
-      // the proper extensions will be added
-      fileName: 'veera',
-    },
     rollupOptions: {},
   },
 });
