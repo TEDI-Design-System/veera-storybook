abef209ceaed91d5261878bddf3c13565d50de08
pearu.sarv <pearu.sarv@twn.ee>
2023-12-04 10:50:50 +0200

    Add Veera logo

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import veeraTheme from './veera-theme';
+
+addons.setConfig({
+  theme: veeraTheme,
+});
---
+++ .storybook/veera-theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+import logo from '../public/veera-logo.png';
+
+export default create({
+  base: 'light',
+  brandImage: logo,
+  brandTitle: 'Veera Storybook'
+});
