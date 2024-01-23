a423da5880afe40dc8c40d30d92ce55c15b54aae
Lilian Väli <lilian.vali@domestack.com>
2024-01-23 15:34:09 +0200

    RIG-5066 Add new logos and links

--- .storybook/veera-theme.js
+++ .storybook/veera-theme.js
@@ -1,5 +1,5 @@
 import { create } from '@storybook/theming/create';
-import logo from '../public/veera-logo.png';
+import logo from '../public/logo_eu_regionaldev.svg';
 
 export default create({
   base: 'light',
